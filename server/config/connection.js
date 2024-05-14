db08c729e367b62fcc9afc74acf1fb22d1c85aed
Alejandro Larios <alexlarios70@hotmail.com>
2024-05-14 00:15:16 -0600

    added port to the db connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,6 +5,7 @@ const  mysql  = require("mysql2/promise");
 const db = mysql.createPool(
     {
         host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
         database:process.env.DB_DATABASE,
         user:process.env.DB_USER,
         password:process.env.DB_PASSWORD,
